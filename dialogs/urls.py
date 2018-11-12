0e13d75e3cfdaf79ea50384552256516ce17ef9c
Denys Galtsev <galtsevdv@gmail.com>
2018-11-12 14:09:19 +0200

    business-rules

--- dialogs/urls.py
+++ dialogs/urls.py
@@ -6,8 +6,9 @@ from dialogs import views
 
 urlpatterns = [
     path('thread/', views.ThreadList.as_view()),
-    path('thread/<int:pk>', views.ThreadDetail.as_view()),
-    path('thread/<int:thread_id>/add_participant', views.add_participant),
+    path('thread/<int:pk>', views.get_thread_view),
+    path('thread/<int:thread_id>/participants', views.add_participant),
+    path('thread/<int:thread_id>/participants/<int:user_id>', views.remove_participant),
     path('thread/<int:thread_id>/remove_participant', views.remove_participant),
     path('thread/<int:thread_id>/messages', views.MessageView.as_view()),
 ]
--- dialogs/views.py
+++ dialogs/views.py
@@ -1,6 +1,6 @@
-from rest_framework import status, exceptions
+from rest_framework import status, exceptions, permissions
 from rest_framework.views import APIView
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 
 
@@ -8,18 +8,26 @@ from dialogs.serializers import ThreadSerializer, MessageSerializer
 from accounts.models import CustomUser
 from dialogs import models
 
+class AdminOnly(permissions.BasePermission):
+    message = "You must be admin to perform this action"
+    def has_permission(self, request, view):
+        return request.user.is_admin()
 
-# class ThreadViewSet(viewsets.ModelViewSet):
-#     queryset = models.Thread.objects.all()
-#     serializer_class = serializers.ThreadSerializer
+class MemberOnly(permissions.BasePermission):
+    message = "You must be member of thread to perform this action"
+    def has_permission(self, request, view):
+        thread_id = request.resolver_match.kwargs.get('thread_id')
+        thread = models.Thread.objects.get(id=thread_id)
+        return thread.is_member(request.user.id)
 
 
 class ThreadList(APIView):
     def get(self, request):
-        threads = models.Thread.objects.all()
+        threads = models.Thread.objects.filter(participants__id=request.user.id)
         serializer = ThreadSerializer(threads, many=True)
         return Response(serializer.data)
 
+    @permission_classes([AdminOnly])
     def post(self, request):
         if not request.user.is_admin():
             raise exceptions.PermissionDenied(detail="Only admin can create threads")
@@ -40,64 +48,50 @@ def get_thread(pk):
         raise Http404
 
 
-class ThreadDetail(APIView):
+@api_view()
+def get_thread_view(request, pk):
+    thread = get_thread(pk)
+    serializer = ThreadSerializer(thread)
+    return Response(serializer.data)
 
-    def get(self, request, pk):
-        thread = get_thread(pk)
-        serializer = ThreadSerializer(thread)
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        thread = get_thread(pk)
-        serializer = ThreadSerializer(thread, data.request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk):
-        thread = get_thread(pk)
-        thread.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
 
 @api_view(['POST'])
+@permission_classes([MemberOnly])
 def add_participant(request, thread_id):
     thread = get_thread(pk=thread_id)
-    if not thread.participants.filter(pk=request.user.id).exists():
-        raise exceptions.PermissionDenied(detail="Only participants can add new participants to the thread")
     participant_id = request.data['id']
     participant = CustomUser.objects.get(pk=participant_id)
+    if participant.is_admin():
+        raise ValidationError(detail="Only one admin allowed for thread")
     thread.participants.add(participant)
     serializer = ThreadSerializer(thread)
     return Response(serializer.data)
 
-@api_view(['POST'])
-def remove_participant(request, thread_id):
+
+@api_view(['DELETE'])
+@permission_classes([AdminOnly, MemberOnly])
+def remove_participant(request, thread_id, user_id):
     thread = get_thread(pk=thread_id)
-    if not request.user.is_admin():
-        raise exceptions.PermissionDenied(detail="Only admin can remove participants from the thread")
-    if not thread.is_member(request.user.id):
-        raise exceptions.PermissionDenied(detail="Only participants can add new participants to the thread")
-    participant_id = request.data['id']
-    participant = CustomUser.objects.get(pk=participant_id)
+    if user_id == request.user.id:
+        thread.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    participant = CustomUser.objects.get(pk=user_id)
     thread.participants.remove(participant)
     serializer = ThreadSerializer(thread)
     return Response(serializer.data)
 
 
 class MessageView(APIView):
+    permission_classes = [MemberOnly]
+
     def get(self, request, thread_id):
         thread = get_thread(thread_id)
-        if not thread.is_member(request.user.id):
-            raise exceptions.PermissionDenied(detail="Only member can view thread messages")
         messages = models.Message.objects.filter(thread__id=thread_id)
         serializer = MessageSerializer(messages, many=True)
         return Response(serializer.data)
 
     def post(self, request, thread_id):
         thread = get_thread(thread_id)
-        if not thread.is_member(request.user.id):
-            raise exceptions.PermissionDenied(detail="Only member can add thread messages")
         # TODO: serializer for Message create
         text = request.data["text"].strip()
         if text:
